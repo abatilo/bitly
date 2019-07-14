0d432e1e4ed6bd149fae36e4cd979b8581d2c0dd
Aaron <AaronBatilo@gmail.com>
2019-07-14 16:27:12 -0600

    Improve pylint score

--- bitly/entrypoint.py
+++ bitly/entrypoint.py
@@ -2,22 +2,30 @@
 Bit.ly Backend Coding Challenge
 """
 
-import asyncio
-import aiohttp
-import json
 import math
 from collections import defaultdict
 from http import HTTPStatus
+
+import asyncio
+import aiohttp
 
 _BITLY_BASE_URL = "https://api-ssl.bitly.com/v4"
 
 
 def flatten(list_of_lists):
+    """
+    Takes a list of lists and flattens everything down to a single list
+    :param list_of_lists: A list where the elements are lists
+    """
     return (item for sublist in list_of_lists for item in sublist)
 
 
 async def fetch_default_group_id(session):
-    async with session.get(_BITLY_BASE_URL + "/user") as response:
+    """
+    Retrieves the default group id for the user that owns the used access token
+    :param session: An aiohttp ClientSession to make requests with
+    """
+    async with session.get(f"{_BITLY_BASE_URL}/user") as response:
         if response.status == HTTPStatus.OK:
             return (await response.json())["default_group_guid"], True
         print(await response.text())
@@ -25,7 +33,12 @@ async def fetch_default_group_id(session):
 
 
 async def fetch_bitlink_ids(session, group_id):
-    first_page = f"https://api-ssl.bitly.com/v4/groups/{group_id}/bitlinks?page=1"
+    """
+    Retrieves a list of all of the bitlink ids that belong to a given group
+    :param session: An aiohttp ClientSession to make requests with
+    :param group_id: The group id whose bitlinks we want to retrieve
+    """
+    first_page = f"{_BITLY_BASE_URL}/groups/{group_id}/bitlinks"
     number_of_concurrent_requests_to_make = 0
 
     responses = []
@@ -41,7 +54,7 @@ async def fetch_bitlink_ids(session, group_id):
 
     coroutines = (
         session.get(
-            f"https://api-ssl.bitly.com/v4/groups/{group_id}/bitlinks",
+            f"{_BITLY_BASE_URL}/groups/{group_id}/bitlinks",
             params={"page": page_number},
         )
         for page_number in range(2, number_of_concurrent_requests_to_make + 1)
@@ -65,12 +78,17 @@ async def fetch_bitlink_ids(session, group_id):
 
 
 async def fetch_clicks_per_country(session, bitlinks, *, unit="day", units=30):
+    """
+    Retrieves the number of bitlink clicks per country
+    :param session: An aiohttp ClientSession to make requests with
+    :param bitlinks: A collection of bitlink ids to use for the aggregation
+    :param unit: The time interval to use to group the click counts
+    :param units: The number of `unit` intervals to query for
+    """
     params = {"unit": unit, "units": units}
 
     coroutines = (
-        session.get(
-            f"https://api-ssl.bitly.com/v4/bitlinks/{bitlink}/countries", params=params
-        )
+        session.get(f"{_BITLY_BASE_URL}/bitlinks/{bitlink}/countries", params=params)
         for bitlink in bitlinks
     )
 
@@ -95,24 +113,28 @@ async def main(loop):
     """
     Main entrypoint of this application
     """
-    h = {"Authorization": "Bearer 2259b2b77fc1bda9ba43555fc983a24cb0104f0e"}
-    async with aiohttp.ClientSession(loop=loop, headers=h) as session:
+    headers = {"Authorization": "Bearer 2259b2b77fc1bda9ba43555fc983a24cb0104f0e"}
+    async with aiohttp.ClientSession(loop=loop, headers=headers) as session:
         group_id, success = await fetch_default_group_id(session)
         if not success:
             print("Problem getting group id")
+            return
 
         bitlinks, success = await fetch_bitlink_ids(session, group_id)
         if not success:
             print("Problem getting bitlink ids")
+            return
 
         click_sums, success = await fetch_clicks_per_country(session, bitlinks)
         if not success:
             print("Problem getting bitlink metrics")
+            return
+
         averaged = {country: clicks / 30 for country, clicks in click_sums.items()}
         print(averaged)
 
 
 if __name__ == "__main__":
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(main(loop))
-    loop.close()
+    LOOP = asyncio.get_event_loop()
+    LOOP.run_until_complete(main(LOOP))
+    LOOP.close()
